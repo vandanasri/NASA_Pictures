d186d861d13daa5323e2380dbb8343477d6f6a30
vandanasri <sri.vandana87@gmail.com>
2020-02-18 14:24:40 +0530

    Injected Application Component in NasaPictureApplication

--- app/src/main/java/com/vandana/nasapictures/NasaPictureApplication.kt
+++ app/src/main/java/com/vandana/nasapictures/NasaPictureApplication.kt
@@ -1,10 +1,24 @@
 package com.vandana.nasapictures
 
 import android.app.Application
+import com.vandana.nasapictures.ui.component.ApplicationComponent
+import com.vandana.nasapictures.ui.component.DaggerApplicationComponent
+import com.vandana.nasapictures.ui.module.ApplicationModule
 
 class NasaPictureApplication : Application(){
 
+    lateinit var applicationComponent: ApplicationComponent
+
     override fun onCreate() {
         super.onCreate()
+        injectDependencies()
+    }
+
+    private fun injectDependencies() {
+        applicationComponent = DaggerApplicationComponent
+            .builder()
+            .applicationModule(ApplicationModule(this))
+            .build()
+        applicationComponent.inject(this)
     }
 }
